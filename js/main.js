431107073c4f23b0093001991ddb22bd2aa37f48
Ignacio Cartes <ignacio.cartes.w@gmail.com>
2018-03-22 19:00:13 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* globals window */
+window.onload = function(event) {
+
+    /* Nothing so far,
+    but here's where you would add javascript code to be executed when the page has loaded */
+    window.console.log("Ready to do fancy stuff!");
+    
+};
